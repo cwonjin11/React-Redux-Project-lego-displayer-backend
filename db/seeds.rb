25503abed18a93dd4877c96dcd0112f78100c562
Wonjin Cho <cwonjin11@gmail.com>
2021-07-23 13:55:36 -0400

    add more seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,18 +13,40 @@ Lego.destroy_all
     Lego.create(
         name: "Taj Mahal",
         item_number: 21056,
-        image: "https://www.lego.com/cdn/cs/set/assets/blt10cbc9ffc0459599/21056.jpg?fit=bounds&format=jpg&quality=80&width=65&height=45&dpr=2",
+        image: "https://www.lego.com/cdn/cs/set/assets/blt5d868260e893b88d/21056.jpg?fit=bounds&format=webply&quality=80&width=320&height=320&dpr=2",
         pieces: 2022,
         prices: 119.99
-
     )
     Lego.create(
         name: "NASA Space Shuttle Discovery",
         item_number: 10283,
-        image: "https://www.lego.com/cdn/cs/set/assets/blt9a603d53c81ac36a/10283_alt1.jpg?fit=bounds&format=jpg&quality=80&width=65&height=45&dpr=2",
+        image: "https://www.lego.com/cdn/cs/set/assets/blt00ba54004c17e820/10283.jpg?fit=bounds&format=webply&quality=80&width=320&height=320&dpr=2",
         pieces: 2354,
         prices: 199.99
+    )
 
+    Lego.create(
+        name: "Pirate Ship",
+        item_number: 31109,
+        image: "https://www.lego.com/cdn/cs/set/assets/blt28eab11b67d4156b/31109.jpg?fit=bounds&format=webply&quality=80&width=320&height=320&dpr=2",
+        pieces: 1260,
+        prices: 99.99
+    )
+
+    Lego.create(
+        name: "Sunset Track Racer",
+        item_number: 31089,
+        image: "https://www.lego.com/cdn/cs/set/assets/blt6d9d7b2fd37e2464/31089.jpg?fit=bounds&format=webply&quality=80&width=320&height=320&dpr=2",
+        pieces: 221,
+        prices: 19.99
+    )
+
+    Lego.create(
+        name: "Deep Sea Creatures",
+        item_number: 31088,
+        image: "https://www.lego.com/cdn/cs/set/assets/bltc691d8d0172af5f1/31088.jpg?fit=bounds&format=webply&quality=80&width=320&height=320&dpr=2",
+        pieces: 230,
+        prices: 14.99
     )
 
 
